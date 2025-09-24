0cfb4701b9416d05ecdddb515a2a37a0bea4fe7f
goudot <Emmanuel.Goudot@gmail.com>
2025-09-24 16:43:24 +0200

    UPD latest

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Bonjour, Render et FastAPI !"}
+    return {"message": "Bonjour, Render et FastAPI, lastest !"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
